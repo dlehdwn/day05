d43563d5171ff0f6785e442759c5500488c298e0
dlehdwn <155503289+dlehdwn@users.noreply.github.com>
2024-01-09 12:54:18 +0900

    Add files via upload

---
+++ day05/quiz.py
@@ -0,0 +1,18 @@
+n = """Inspections began after a section of the fuselage fell from an Alaska Airlines 737 Max 9 on Friday.
+
+United Airlines said "installation issues" relating to door plugs would be "remedied" before the aircraft type would return to service.
+
+Some 171 planes of the same type remain grounded by the US regulator.
+
+In its statement, United said: "Since we began preliminary inspections on Saturday, we have found instances that appear to relate to installation issues in the door plug - for example, bolts that needed additional tightening."
+
+The door plug is a piece of fuselage with a window that can be used as an emergency exit in certain configurations.
+
+It was this part of the Alaska Airlines plane which dramatically fell off mid-flight over the US state of Oregon, eventually landing in a teacher's back garden.
+
+The plane made an emergency landing and none of the passengers were injured."""
+a = n.split()
+a.sort()
+print(a)
+
+
---
+++ day05/review_quiz.py
@@ -0,0 +1,34 @@
+# 여행 계획표 만들기
+# a = str(input("일본 여행 계획을 입력해 주세요:"))
+# print(f"일본 여행 계획표는 {a.split(',')}입니다")
+#
+# # 기사에서 단어 수 찾기
+# n = """Bolts in need of "additional tightening" have been found during inspections of Boeing 737 Max 9s, United Airlines has said.
+#
+# Inspections began after a section of the fuselage fell from an Alaska Airlines 737 Max 9 on Friday.
+#
+# United Airlines said "installation issues" relating to door plugs would be "remedied" before the aircraft type would return to service.
+#
+# Some 171 planes of the same type remain grounded by the US regulator.
+#
+# In its statement, United said: "Since we began preliminary inspections on Saturday, we have found instances that appear to relate to installation issues in the door plug - for example, bolts that needed additional tightening."
+#
+# The door plug is a piece of fuselage with a window that can be used as an emergency exit in certain configurations.
+#
+# It was this part of the Alaska Airlines plane which dramatically fell off mid-flight over the US state of Oregon, eventually landing in a teacher's back garden.
+#
+# The plane made an emergency landing and none of the passengers were"""
+# q = input("찾고싶은 단어를 입력하세요:")
+# print(f"단어의 개수는 {n.count(q)}개 입니다")
+#
+# # 스벅 메뉴 선택
+# a = int(input("커피를 고르세요['1번 아메리카노 1500원', '2번 카페라떼 2000원', '3번 바닐라라떼 2000원'] : "))
+# b = int(input("디저트를 고르세요['1번 조각케익 3000원', '2번 카스테라 2000원', '3번 쿠키 2000원'] : "))
+# x = ['0', '1번 아메리카노 1500원', '2번 카페라떼 2000원', '3번 바닐라라떼 2000원']
+# y = ['0', '1번 조각케익 3000원', '2번 카스테라 2000원', '3번 쿠키 2000원']
+# z = [0,1500,2000,2000]
+# r = [0,3000,2000,2000]
+# print(f"고르신 커피는 {x[a]}이고, 디저트는 {y[b]}입니다 / 계산하실 가격은 {(z[a]) + (r[b])}원입니다 ")
+
+
+
